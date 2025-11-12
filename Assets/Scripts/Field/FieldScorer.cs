d8f52cfef876981a90ec28080176cc80017a751d
masonmm3 <53064865+masonmm3@users.noreply.github.com>
2025-11-12 12:44:49 -0500

    Fixed the barge scoring finall

--- Assets/Scripts/Field/FieldScorer.cs
+++ Assets/Scripts/Field/FieldScorer.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using MyBox;
 using UnityEngine;
 using Util;
 
 public class FieldScorer : MonoBehaviour
 {
+    [Tooltip("this will display a blue box around the scoring node when in the editor")]
+    [SerializeField] private bool displayDebugBox = false;
     [SerializeField] private bool isBlue;
     [SerializeField] private int scoreToAdd;
     [SerializeField] private int autoScoreToAdd;
@@ -28,11 +31,39 @@ public class FieldScorer : MonoBehaviour
         occupyObjects = new List<GamePiece>();
         scoredInAuto = 0;
         halfExtents = new Vector3[occupyColliders.Length];
-        for (int i = 0; i < occupyColliders.Length; i++)
-        {
-            halfExtents[i] = occupyColliders[i].bounds.extents / 2;
-        }
         scorePiecesSet.Clear();
+        
+        foreach (var coll in occupyColliders) 
+        {
+            Vector3 localHalfExtents = Vector3.zero;
+            int index = occupyColliders.IndexOfItem(coll);
+
+            if (coll is BoxCollider boxCollider)
+            {
+                localHalfExtents = boxCollider.size / 2f;
+            }
+            else if (coll is CapsuleCollider capsuleCollider)
+            {
+                switch (capsuleCollider.direction)
+                {
+                    case 0://x
+                        localHalfExtents = new Vector3(capsuleCollider.height / 2f, capsuleCollider.radius, capsuleCollider.radius);
+                        break;
+                    case 1://y
+                        localHalfExtents = new Vector3(capsuleCollider.radius, capsuleCollider.height / 2f, capsuleCollider.radius);
+                        break;
+                    case 2://z
+                        localHalfExtents = new Vector3(capsuleCollider.radius, capsuleCollider.radius, capsuleCollider.height / 2f);
+                        break;
+                }
+            }
+            
+            if (index >= 0 && index < halfExtents.Length)
+            {
+                halfExtents[index] = localHalfExtents;
+            }
+        }
+        
         foreach (var name in scorePieces)
         {
             scorePiecesSet.Add(name);
@@ -82,15 +113,13 @@ public class FieldScorer : MonoBehaviour
         uniquePieces.Clear();
         pieces.Clear();
         
-        for (int i = 0; i < occupyColliders.Length; i++)
-        {
-            var overlapBox = Physics.OverlapBox(occupyColliders[i].transform.position, halfExtents[i], occupyColliders[i].transform.rotation, peiceMask);
-            foreach (var collider in overlapBox)
+        foreach (var coll in occupyColliders) {
+            var overlapBox = Physics.OverlapBox(coll.gameObject.transform.position, halfExtents[occupyColliders.IndexOfItem(coll)], coll.gameObject.transform.rotation, peiceMask);
+            foreach (var box in overlapBox)
             {
-                var objectThing = collider.gameObject;
-                var piece = Utils.FindParentObjectComponent<GamePiece>(objectThing);
-                //if (!piece) continue;
-                if (!scorePieces.Contains(piece.pieceType)) continue;
+                var piece = Utils.FindParentObjectComponent<GamePiece>(box.gameObject); 
+                if (!piece) continue;
+                if (!scorePiecesSet.Contains(piece.pieceType)) continue;
                 if (uniquePieces.Contains(piece)) continue;
                 if (piece.state != GamePieceState.World) continue;
                 uniquePieces.Add(piece);
@@ -99,5 +128,44 @@ public class FieldScorer : MonoBehaviour
 
         pieces.AddRange(uniquePieces);
         return pieces;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!displayDebugBox) return;
+        if (occupyColliders == null || halfExtents == null) return;
+
+        // Use a distinguishable color (e.g., Green/Cyan, similar to your desired box)
+        Gizmos.color = new Color(0f, 0f, 1f, 0.6f); // Greenish-Cyan, semi-transparent
+
+        for (int i = 0; i < occupyColliders.Length; i++)
+        {
+            Collider coll = occupyColliders[i];
+
+            if (i >= halfExtents.Length || coll == null) continue;
+
+            // 1. Get the necessary transformation data
+            Transform collTransform = coll.gameObject.transform;
+            Vector3 position = collTransform.position;
+            Quaternion rotation = collTransform.rotation;
+            Vector3 halfExtent = halfExtents[i]; // This MUST be the local half-extents
+
+            // 2. Apply the same transformation matrix used in OverlapBox
+            Matrix4x4 originalMatrix = Gizmos.matrix;
+
+            // The TRS (Translate, Rotate, Scale) matrix applies the object's position and rotation
+            Gizmos.matrix = Matrix4x4.TRS(position, rotation, Vector3.one);
+
+            // 3. Draw the cube relative to the new matrix
+            // We draw at Vector3.zero because the matrix already includes the position.
+            // We multiply halfExtent by 2 to get the full size required by DrawWireCube.
+            Gizmos.DrawWireCube(Vector3.zero, halfExtent * 2f);
+
+            // Optionally, draw a solid cube to better visualize the volume
+            // Gizmos.DrawCube(Vector3.zero, halfExtent * 2f);
+
+            // 4. Restore the original Gizmos matrix
+            Gizmos.matrix = originalMatrix;
+        }
     }
 }
